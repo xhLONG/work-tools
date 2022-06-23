741f0fc83ebd84fddbc0f3f8a41b12597f224cc0
xhLONG <1874596122@qq.com>
2022-06-23 17:44:38 +0800

    feat:随便

--- src/components/ScrollBar.vue
+++ src/components/ScrollBar.vue
@@ -27,14 +27,10 @@
         width: 6px;
         /*对水平流动条有效*/
         height: 6px;
-        background-color: #73d2f3;
     }
     /*定义滚动条的轨道颜色、内阴影及圆角*/
     .box::-webkit-scrollbar-track {
-        -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .2);
-        background: linear-gradient(to top, #fcf5ee, #faf7e6, #73d2f3, #eeeeee);
-        border-radius: 1px;
-        // display: none;
+        background: #c5c7d1 ;
     }
     /*定义滑块颜色、内阴影及圆角*/
     .box::-webkit-scrollbar-thumb {
@@ -47,12 +43,12 @@
         background-color: #e66464;
     }
     /*定义右下角汇合处的样式*/
-    .box::-webkit-scrollbar-corner {
-        background: #ffffff;
-    }
-    .box::-webkit-scrollbar-track-piece {
-        background-color: #ffffff;
-    }
+    // .box::-webkit-scrollbar-corner {
+    //     background: #ffffff;
+    // }
+    // .box::-webkit-scrollbar-track-piece {
+    //     background-color: #ffffff;
+    // }
 
     .inner{
         height: 200px;
